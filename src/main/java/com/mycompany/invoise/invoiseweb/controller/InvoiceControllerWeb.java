07631fa3dd950d1c671e409bf37d91c1e7d0a4e4
D-Rudy <duriez.ry@gmail.com>
2023-02-25 13:16:04 +0100

    Init

---
+++ src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
@@ -0,0 +1,41 @@
+package com.mycompany.invoise.invoiseweb.controller;
+
+import com.mycompany.invoise.core.controller.InvoiceControllerInterface;
+import com.mycompany.invoise.core.entity.Invoice;
+import com.mycompany.invoise.core.service.InvoiceServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class InvoiceControllerWeb implements InvoiceControllerInterface {
+
+    @Autowired
+    private InvoiceServiceInterface invoiceService;
+
+    public InvoiceServiceInterface getInvoiceService() {
+        return invoiceService;
+    }
+
+    public void setInvoiceService(InvoiceServiceInterface invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    public void createInvoice() {
+        String customerName = "Michel";
+        Invoice invoice = new Invoice();
+        invoice.setCustomerName(customerName);
+
+        invoiceService.createInvoice(invoice);
+    }
+
+    @RequestMapping("/invoice-home")
+    public @ModelAttribute("invoiceList") List<Invoice> displayHome() {
+        System.out.println("Hello from displayHome");
+        List<Invoice> invoiceList = invoiceService.getInvoiceList();
+        return invoiceList;
+    }
+}
